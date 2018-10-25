11497e325bb4856c85b5c737e55af9025963cb28
Thierry Huu <huuth@macbook-pro-de-thierry.local>
2018-10-25 11:46:01 +0200

    UIAlertController avec actionSheet

--- Appareil photo 1/ViewController.swift
+++ Appareil photo 1/ViewController.swift
@@ -21,8 +21,46 @@ class ViewController: UIViewController {
         imagePicker.delegate = self
         imagePicker.allowsEditing = true
     }
+    
+    func presentWithSource(_ source: UIImagePickerController.SourceType) {
+        imagePicker.sourceType = source
+        present(imagePicker, animated: true, completion: nil)
+    }
 
     @IBAction func shootAction(_ sender: UIButton) {
+        let alertActionSheet = UIAlertController(title: "Prendre une photo", message: "Choisissez le média", preferredStyle: .actionSheet)
+        
+        let camera = UIAlertAction(title: "Appareil photo", style: .default) { (action) in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                self.presentWithSource(.camera)
+            } else {
+                let alerte = UIAlertController(title: "Erreur", message: "Aucun appareil photo n'est disponible", preferredStyle: .alert)
+                
+                let annuler = UIAlertAction(title: "Je comprends", style: .destructive, handler: nil)
+                
+                alerte.addAction(annuler)
+                self.present(alerte, animated: true, completion: nil)
+            }
+        }
+        
+        let gallery = UIAlertAction(title: "Gallerie de photos", style: .default) { (action) in
+            self.presentWithSource(.photoLibrary)
+        }
+        
+        let cancel = UIAlertAction(title: "Annuler", style: .cancel, handler: nil)
+        
+        alertActionSheet.addAction(camera)
+        alertActionSheet.addAction(gallery)
+        alertActionSheet.addAction(cancel)
+        
+        // Pour un iPad
+        if let popover = alertActionSheet.popoverPresentationController {
+            popover.sourceView = view
+            popover.sourceRect = CGRect(x: view.frame.midX, y: view.frame.midY, width: 0, height: 0)
+            popover.permittedArrowDirections = []
+        }
+        
+        present(alertActionSheet, animated: true, completion: nil)
     }
 }
 
