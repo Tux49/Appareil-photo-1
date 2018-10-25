69fbab38c9ab64ff9ad6b24c36d4ed9aaf1eb94c
Thierry Huu <huuth@macbook-pro-de-thierry.local>
2018-10-25 10:51:39 +0200

    Mise en place du projet

--- Appareil photo 1/ViewController.swift
+++ Appareil photo 1/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chosedImageView: UIImageView!
+    @IBOutlet weak var noImageLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func shootAction(_ sender: UIButton) {
+    }
 }
 
